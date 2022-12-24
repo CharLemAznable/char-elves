cf9c7139c383537855d94890e374234dd6438177
CharLemAznable <545541819@qq.com>
2022-12-24 19:39:06 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/core/spring/ElvesAutoConfiguration.java
+++ src/main/java/com/github/charlemaznable/core/spring/ElvesAutoConfiguration.java
@@ -3,7 +3,6 @@ package com.github.charlemaznable.core.spring;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@SuppressWarnings("SpringFacetCodeInspection")
 @Configuration
 @NeoComponentScan
 @Import({SpringContextRegistrar.class})
