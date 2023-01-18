94d4f99cd35ffefe3da5215d13f2af457460328f
CharLemAznable <545541819@qq.com>
2023-01-19 01:36:55 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/core/vertx/spring/VertxConfiguration.java
+++ src/main/java/com/github/charlemaznable/core/vertx/spring/VertxConfiguration.java
@@ -2,9 +2,9 @@ package com.github.charlemaznable.core.vertx.spring;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 
 import static com.github.charlemaznable.core.lang.Condition.nullThen;
 import static com.github.charlemaznable.core.vertx.VertxElf.buildVertx;
@@ -13,7 +13,7 @@ import static com.github.charlemaznable.core.vertx.VertxElf.buildVertx;
 public class VertxConfiguration {
 
     @Bean
-    public Vertx vertx(@Autowired(required = false) VertxOptions vertxOptions) {
+    public Vertx vertx(@Nullable VertxOptions vertxOptions) {
         return buildVertx(nullThen(vertxOptions, VertxOptions::new));
     }
 }
